b0bdea89f3dd02dd301626ddf0137cfdd5ebfda6
Sunday Brian <brians931@gmail.com>
2019-07-27 17:46:07 +0300

    refactored the app to display the news sources with the categories

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import render_template
 from app import app
 from .request import get_sources
 
-#views
+# views
 @app.route('/')
 @app.route('/watchdogs')
 def index():
@@ -10,19 +10,19 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    #Getting the news sources
-    business_sources=get_sources('business')
-    entertainment_sources=get_sources('entertainment')
-    general_sources=get_sources('general')
-    health_sources=get_sources('health')
-    science_sources=get_sources('science')
-    sports_sources=get_sources('sports')
-    tech_sources=get_sources('technology')
-
-    print(news_sources)
+    # Getting the news sources
+    business_sources = get_sources('business')
+    entertainment_sources = get_sources('entertainment')
+    general_sources = get_sources('general')
+    health_sources = get_sources('health')
+    science_sources = get_sources('science')
+    sports_sources = get_sources('sports')
+    tech_sources = get_sources('technology')
 
     title = 'WatchDogs News App'
-    return render_template('index.html',title=title,source=news_sources)
+    return render_template('index.html', title=title, business=business_sources,
+                           entertainment=entertainment_sources, general=general_sources, health=health_sources,
+                           science=science_sources, sports=sports_sources, tech=tech_sources)
 
 
 @app.route("/watchdogs/<source_id>")
@@ -30,4 +30,4 @@ def news_source(source_id):
     '''
     View new_source page function that returns a news source page and its data
     '''
-    return render_template('newsSource.html',id =source_id)
+    return render_template('newsSource.html', id=source_id)
