9f1374008bd68ac08144698fda8240ea9082d1a6
Sunday Brian <brians931@gmail.com>
2019-07-28 21:18:11 +0300

    moved the config.py to the root directory

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 class Config:
     '''
     General Configuration parent class
@@ -25,5 +27,11 @@ class DevConfig(Config):
         Config:The parent configuration class with General configuration settings
     '''
     DEBUG = True
+
+
+config_options={
+    'development':DevConfig,
+    'production':ProdConfig
+}    
 
 
